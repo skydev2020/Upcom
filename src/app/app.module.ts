f61971a134e90a5adf277b34726f2fbe6766a101
skydev2020 <skydev2020@outlook.com>
2021-02-19 01:37:15 -0500

    Angular Upcom update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { BioComponent } from './bio/bio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    BioComponent
   ],
   imports: [
     BrowserModule,
