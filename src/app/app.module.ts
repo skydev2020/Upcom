3f02f6c3bcc521173b6352dd4e30432d31d01230
skydev2020 <skydev2020@outlook.com>
2021-02-19 03:25:55 -0500

    add new module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import {FooModule } from './foo/foo.module';
+
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { BioComponent } from './bio/bio.component';
+import { FooComponent } from './foo/foo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
-    BioComponent
+    BioComponent,
+    FooComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FooModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
